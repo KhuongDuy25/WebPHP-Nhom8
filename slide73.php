176e029ff71f04c55101aa9533d178c4e5904f5c
Waregkun <124433008+Waregkun@users.noreply.github.com>
2024-04-19 15:12:32 +0700

    Add files via upload
    bài tập slide 73

---
+++ slide73.php
@@ -0,0 +1,16 @@
+<html>
+    <?php
+    for($i = 1;$i<=200;$i++)
+    {
+
+    if($i%2==0)
+
+    echo "<b><font color=red>". $i." - " . "</font></b>";
+
+    else
+
+    echo "<i><font color=blue>". $i." - " . "</font></i>";
+
+    }
+    ?>
+</html> 
